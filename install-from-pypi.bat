b9bf2031cbdfe9a896452fb4024e1515d7b0a71a
Cologler <skyoflw@gmail.com>
2018-01-04 18:44:15 +0800

    refactor

---
+++ install-from-pypi.bat
@@ -0,0 +1,1 @@
+pip install fsoopify --upgrade
---
+++ test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+import os
+import sys
+import traceback
+import unittest
+from fsoopify import (
+    Path,
+    NodeInfo, DirectoryInfo, FileInfo,
+)
+
+class Test(unittest.TestCase):
+    def test_node(self):
+        NodeInfo.from_path('.').list_items
+
+    def test_path(self):
+        rpath = r'D:\Projects-Arc\fsoopify-python.xx'
+        path = Path(rpath)
+
+        self.assertEqual(path, rpath)
+        self.assertEqual(path.normalcase, os.path.normcase(rpath))
+        self.assertTrue(path.equals(rpath.upper()))
+
+        self.assertEqual(path.dirname, os.path.dirname(rpath))
+        self.assertEqual(path.name, os.path.split(rpath)[1])
+        self.assertEqual(path.name.pure_name, os.path.splitext(os.path.split(rpath)[1])[0])
+        self.assertEqual(path.name.ext, os.path.splitext(os.path.split(rpath)[1])[1])
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        unittest.main()
+    except Exception:
+        traceback.print_exc()
+
+if __name__ == '__main__':
+    main()
