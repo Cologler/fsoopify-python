08780e14c8b291894a84873aa0f994ec8f7002a8
Cologler <skyoflw@gmail.com>
2019-04-18 11:26:40 +0800

    feat: load pipfile

--- tests/test_file.py
+++ tests/test_file.py
@@ -6,43 +6,13 @@
 #
 # ----------
 
-from pytest import raises
-
 from fsoopify import (
     NodeType,
-    DirectoryInfo, FormatNotFoundError, FileInfo
+    DirectoryInfo, FileInfo
 )
 
 def test_dir_node_type():
-    assert FileInfo('.').node_type is NodeType.file
+    assert DirectoryInfo('.').node_type is NodeType.dir
 
-test_data_dir = DirectoryInfo('test_data_dir')
-test_data_dir.ensure_created()
-
-example_data_1 = {
-    'a': 1,
-    'b': '2',
-    'c': {
-        'd': 'ddddd'
-    }
-}
-
-def test_dump_load_with_format():
-    for fmt in ('json', 'json5', 'yaml', 'toml', 'pickle'):
-        file_info = test_data_dir.get_fileinfo(f'test_data_1_{fmt}.{fmt}')
-        file_info.dump(example_data_1, fmt)
-        assert example_data_1 == file_info.load(fmt)
-
-def test_dump_load_with_auto_detect_format():
-
-    for ext in ('json', 'json5', 'yaml', 'toml'):
-        file_info = test_data_dir.get_fileinfo(f'test_data_2.{ext}')
-        file_info.dump(example_data_1)
-        assert example_data_1 == file_info.load()
-
-    for fmt in ('pickle', ):
-        fi = test_data_dir.get_fileinfo(f'test_data_2_{fmt}.{fmt}')
-        with raises(FormatNotFoundError):
-            fi.dump(example_data_1)
-        with raises(FormatNotFoundError):
-            fi.load()
+def test_file_node_type():
+    assert FileInfo('abc').node_type is NodeType.file
