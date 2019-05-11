727a0b7e752e0a5b978b5ae58bf1549279b515b4
Cologler <skyoflw@gmail.com>
2019-05-11 08:05:59 +0800

    add tests

--- tests/test_path_abs.py
+++ tests/test_path_abs.py
@@ -14,13 +14,18 @@ from fsoopify import Path
 
 NT = sys.platform == 'win32'
 
-def test_get_parent():
+def test_abspath_get_parent():
     path = Path.from_argv(0)
     parent = path.get_parent()
     assert parent == os.path.dirname(sys.argv[0])
 
+def _assert_root_abspath_unable_get_parent(path: Path):
+    assert path.is_abspath()
+    with pytest.raises(ValueError, match='max level is 0'):
+        path.get_parent()
+
 @pytest.mark.skipif(not NT, reason="only run on windows")
-def test_get_parent_win32():
+def test_abspath_get_parent_win32():
     src_path = os.path.join('c:\\', 'd', 'e')
     path = Path(src_path)
     assert path.get_parent(1) == path.dirname
@@ -29,7 +34,12 @@ def test_get_parent_win32():
     with pytest.raises(ValueError, match='max level is 2'):
         path.get_parent(3)
 
+    # test root:
+    _assert_root_abspath_unable_get_parent(Path('C:'))
+    _assert_root_abspath_unable_get_parent(Path('C:\\'))
+
 @pytest.mark.skipif(NT, reason="only run on unix")
-def test_get_parent_unix():
-    # TODO
-    pass
+def test_abspath_get_parent_unix():
+
+    # test root:
+    _assert_root_abspath_unable_get_parent(Path('/'))
