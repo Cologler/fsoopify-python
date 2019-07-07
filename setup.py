7271fef7a4d961b36e3b1c48534220bac47cd895
Cologler <skyoflw@gmail.com>
2019-07-07 22:04:34 +0800

    switch to use setupmeta_builder

--- setup.py
+++ setup.py
@@ -6,35 +6,8 @@
 #
 # ----------
 
-import os
-from setuptools import setup, find_packages
+from setupmeta_builder import setup_it
 
-VERSION = '0.2.4'
-DESCRIPTION = ''
-
-long_description = None
-
-if os.path.isfile('README.md'):
-    with open('README.md') as f:
-        long_description = f.read()
-
-long_description = long_description or DESCRIPTION
-
-setup(
-    name = 'fsoopify',
-    version = VERSION,
-    description = DESCRIPTION,
-    long_description = long_description,
-    long_description_content_type='text/markdown',
-    classifiers = [],
-    keywords = ['python', 'fs', 'oop', 'oopify', 'filesystem'],
-    author = 'Cologler',
-    author_email='skyoflw@gmail.com',
-    url = 'https://github.com/Cologler/fsoopify-python',
-    license = 'MIT',
-    packages = find_packages(),
-    include_package_data = True,
-    install_requires = [],
-    zip_safe = False,
-    entry_points = {}
+setup_it(
+    keywords = ['python', 'fs', 'oop', 'oopify', 'filesystem']
 )
