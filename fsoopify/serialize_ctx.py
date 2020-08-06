a62307d2955e80fa9c697a094b16b90ece9f75bc
Cologler <skyoflw@gmail.com>
2020-08-06 13:05:07 +0800

    feat: test with errors

--- fsoopify/serialize_ctx.py
+++ fsoopify/serialize_ctx.py
@@ -13,40 +13,78 @@ class Context:
     data = None
     save_on_exit = True
 
+    def __init__(self, file_info, *, serializer, load_kwargs: dict, dump_kwargs: dict, lock: bool):
+        super().__init__()
+        self._file_info = file_info
+        self._serializer = serializer
+        self._load_kwargs = load_kwargs
+        self._dump_kwargs = dump_kwargs
+        self._lock = lock
+        # states:
+        self._lock_ctx = None
+        self._fp = None
+
+    def __enter__(self):
+        is_exists = self._file_info.is_file()
+        self._fp = self._file_info.open('r+b' if is_exists else 'wb')
+        self._lock_ctx = self._lock(self._file_info, self._fp)
+        self._lock_ctx.__enter__()
+        if is_exists:
+            self.data = self._serializer.loadf(self._fp, options={
+                'origin_kwargs': self._load_kwargs
+            })
+        else:
+            self.data = None
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            if exc_val is None and self.save_on_exit:
+                assert self._fp is not None
+                if self.data is None:
+                    self._fp.close()
+                    if self._file_info.is_file():
+                        self._file_info.delete()
+                else:
+                    buf = self._serializer.dumpb(self.data, options={
+                        'origin_kwargs': self._dump_kwargs
+                    })
+                    self._fp.seek(0)
+                    self._fp.write(buf)
+                    self._fp.truncate()
+                    self._fp.close()
+                self._fp = None
+
+        finally:
+            if self._lock_ctx is not None:
+                self._lock_ctx.__exit__(exc_type, exc_val, exc_tb)
+            if self._fp is not None:
+                # possible removed if data is None
+                self._fp.close()
+                self._fp = None
+
 @contextmanager
+def lock_with_nop(fi, fp):
+    yield
+
+@contextmanager
+def lock_with_portalocker(fi, fp):
+    import portalocker
+    portalocker.lock(fp, portalocker.LOCK_EX)
+    yield
+
 def load_context(f, format: str=None, *, load_kwargs: dict, dump_kwargs: dict, lock: bool):
     if lock:
-        import portalocker
+        if lock is True:
+            lock = lock_with_portalocker
+    else:
+        lock = lock_with_nop
 
     serializer = get_serializer(f, format)
 
-    is_exists = f.is_file()
-    fp = f.open('r+b' if is_exists else 'wb')
-    if lock:
-        portalocker.lock(fp, portalocker.LOCK_EX)
-    if is_exists:
-        data = serializer.loadf(fp, options={
-            'origin_kwargs': load_kwargs.copy()
-        })
-    else:
-        data = None
-
-    ctx = Context()
-    ctx.data = data
-    yield ctx
-    data = ctx.data
-
-    if ctx.save_on_exit:
-        if data is not None:
-            options={
-                'origin_kwargs': dump_kwargs.copy()
-            }
-            buf = serializer.dumpb(data, options=options)
-            fp.seek(0)
-            fp.write(buf)
-            fp.truncate()
-            fp.close()
-        else:
-            fp.close()
-            if f.is_file():
-                f.delete()
+    ctx = Context(f,
+        serializer=serializer,
+        load_kwargs=load_kwargs, dump_kwargs=dump_kwargs,
+        lock=lock
+    )
+    return ctx
--- tests/test_file_serialize.py
+++ tests/test_file_serialize.py
@@ -6,6 +6,7 @@
 # ----------
 
 import tempfile
+import contextlib
 
 from pytest import raises
 
@@ -97,6 +98,29 @@ def test_load_context_with_locked():
         with file_info.load_context(lock=True) as s:
             s.data = None
         assert not file_info.is_exists()
+
+def test_load_context_with_error():
+    data = example_data_1
+    name = 'test_load_context.json'
+    with tempfile.TemporaryDirectory() as tmpdir:
+        dir_info = DirectoryInfo(tmpdir)
+        file_info = dir_info.get_fileinfo(name)
+        assert not file_info.is_exists()
+
+        with file_info.load_context(lock=True) as s:
+            assert s.data is None
+            s.data = data
+        assert file_info.is_exists()
+
+        with contextlib.suppress(ValueError):
+            with file_info.load_context(lock=True) as s:
+                assert s.data == data
+                s.data = {}
+                raise ValueError # raise any error
+
+        with file_info.load_context(lock=True) as s:
+            assert s.data == data # still not changes
+
 
 def test_pipfile():
     import pipfile
