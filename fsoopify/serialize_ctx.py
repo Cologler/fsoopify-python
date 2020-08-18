cf31c1b7a6dbfd54ec8e1186eb2cbbfdf3ddb0a1
Cologler <skyoflw@gmail.com>
2020-08-18 18:55:45 +0800

    refactor

--- fsoopify/serialize_ctx.py
+++ fsoopify/serialize_ctx.py
@@ -8,7 +8,19 @@
 import sys
 from contextlib import contextmanager, suppress
 
+import portalocker
+import filelock
+
 from .serialize import *
+
+
+@contextmanager
+def lock_with_portalocker(fp):
+    'use `portalocker.lock` as context manager.'
+    import portalocker
+    portalocker.lock(fp, portalocker.LOCK_EX)
+    yield
+
 
 class Context:
     data = None
@@ -23,9 +35,9 @@ class Context:
         self._lock = lock
         self._atomic = atomic
         # states:
-        self._lock_ctx = None
-        self._openfd_ctx = None
-        self._fd = None
+        self._lock_cm = None
+        self._fp_cm = None
+        self._fp = None
 
     def __enter__(self):
         def _read_data_from(fp):
@@ -33,86 +45,71 @@ class Context:
                 'origin_kwargs': self._load_kwargs
             })
 
+        is_exists = self._file_info.is_file()
+        # we did not want to copy data from src on atomic mode
+        mode = 'wb' if self._atomic else 'r+b'
+
         try:
-            is_exists = self._file_info.is_file()
+            if self._lock and self._atomic:
+                self._lock_cm = filelock.FileLock(self._file_info.path + '.lock')
+                self._lock_cm.__enter__()
 
-            self._openfd_ctx = self._file_info.open('r+b', atomic=self._atomic, or_create=True)
+            self._fp_cm = self._file_info.open(mode, atomic=self._atomic, or_create=True)
+            self._fp = self._fp_cm.__enter__()
 
-            self._fd = self._openfd_ctx.__enter__()
-
-            self._lock_ctx = self._lock(self._file_info, self._fd)
-            self._lock_ctx.__enter__()
+            if self._lock and not self._atomic:
+                self._lock_cm = lock_with_portalocker(self._fp)
+                self._lock_cm.__enter__()
 
             if is_exists:
                 if self._atomic:
                     with self._file_info.open_for_read_bytes() as fp:
                         _read_data_from(fp)
                 else:
-                    self._fd.seek(0)
-                    _read_data_from(self._fd)
+                    self._fp.seek(0)
+                    _read_data_from(self._fp)
 
-        except Exception:
+        except:
             self._cleanup()
             raise
 
         return self
 
     def _cleanup(self):
-        if self._lock_ctx is not None:
-            self._lock_ctx.__exit__(*sys.exc_info())
-            self._lock_ctx = None
+        if self._lock_cm is not None:
+            self._lock_cm.__exit__(*sys.exc_info())
+            self._lock_cm = None
 
-        if self._fd is not None:
-            assert self._openfd_ctx is not None
-            self._fd = None
+        if self._fp is not None:
+            assert self._fp_cm is not None
+            self._fp = None
 
-        if self._openfd_ctx is not None:
-            self._openfd_ctx.__exit__(*sys.exc_info())
-            self._openfd_ctx = None
+        if self._fp_cm is not None:
+            self._fp_cm.__exit__(*sys.exc_info())
+            self._fp_cm = None
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         remove_file = False
         try:
             # only save if no exceptions:
-            if exc_val is None and self.save_on_exit and self._fd:
-                self._fd.seek(0)
+            if exc_val is None and self.save_on_exit and self._fp:
+                self._fp.seek(0)
                 if self.data is None:
                     remove_file = True
                 else:
                     buf = self._serializer.dumpb(self.data, options={
                         'origin_kwargs': self._dump_kwargs
                     })
-                    self._fd.write(buf)
-                self._fd.truncate()
+                    self._fp.write(buf)
+                self._fp.truncate()
         finally:
             self._cleanup()
 
         if remove_file and self._file_info.is_file():
             self._file_info.delete()
 
-@contextmanager
-def lock_with_nop(fi, fp):
-    yield
-
-@contextmanager
-def lock_with_portalocker(fi, fp):
-    import portalocker
-    portalocker.lock(fp, portalocker.LOCK_EX)
-    yield
-
-@contextmanager
-def lock_with_filelock(fi, fp):
-    import filelock
-    with filelock.FileLock(fi.path + '.lock'):
-        yield
 
 def load_context(f, format: str=None, *, load_kwargs: dict, dump_kwargs: dict, lock: bool, atomic: bool):
-    if lock:
-        if lock is True:
-            lock = lock_with_filelock if atomic else lock_with_portalocker
-    else:
-        lock = lock_with_nop
-
     serializer = get_serializer(f, format)
 
     ctx = Context(f,
